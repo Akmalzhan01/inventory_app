f76d05ca6dfa38fb211b84958e9e013d5392217b
Akmalzhan01 <akmalzhantokhtasinov@gmail.com>
2025-05-07 11:46:45 +0600

    setting mongo src

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const morgan = require('morgan');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*"
+})
+);
 app.use(morgan('dev'));
 app.use(express.json());
 
